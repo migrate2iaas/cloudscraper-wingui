5170088edd54fa56dc813252cf8ae832ce84cef1
nobody <nobody@31ede11f-2d93-4789-83b2-5cb4e8e47746>
2013-03-11 09:23:05 +0000

    Added drives selection in S2 form.

--- CloudScraper/CloudScraper/ChooseCloudForm.cs
+++ CloudScraper/CloudScraper/ChooseCloudForm.cs
@@ -10,9 +10,32 @@ namespace CloudScraper
 {
     public partial class ChooseCloudForm : Form
     {
-        public ChooseCloudForm()
+        ChooseDisksForm chooseDiskForm_;
+        CloudParametersForm cloudParametersForm_;
+        
+        public ChooseCloudForm(ChooseDisksForm chooseDiskForm)
         {
+            this.chooseDiskForm_ = chooseDiskForm;
+
             InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.chooseDiskForm_.Show();
+        }
+
+        private void amazonButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            if (this.cloudParametersForm_ == null)
+            {
+                this.cloudParametersForm_ = new CloudParametersForm(this);
+            }
+
+            cloudParametersForm_.ShowDialog();
         }
     }
 }
--- CloudScraper/CloudScraper/CloudParametersForm.Designer.cs
+++ CloudScraper/CloudScraper/CloudParametersForm.Designer.cs
@@ -28,11 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(13, 227);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "<Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // CloudParametersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.backButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CloudParametersForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CloudParametersForm";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button backButton;
     }
 }
