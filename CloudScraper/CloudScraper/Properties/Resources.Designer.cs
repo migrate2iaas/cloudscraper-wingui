adbdf9ac2739ac768817175829f1f5d889e685ad
Vladimir Fedorov <7068534+feoff3@users.noreply.github.com>
2013-10-17 16:01:17 +0000

    merged with trunk

--- CloudScraper/CloudScraper/Properties/Resources.Designer.cs
+++ CloudScraper/CloudScraper/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace CloudScraper.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ajax_loader_1_ {
+            get {
+                object obj = ResourceManager.GetObject("ajax-loader(1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ajax_loader20s {
             get {
                 object obj = ResourceManager.GetObject("ajax-loader20s", resourceCulture);
