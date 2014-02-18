3ce2b6f72b8be93da5496cc605e1632a5ea0c6dd
feoff <feoff@31ede11f-2d93-4789-83b2-5cb4e8e47746>
2014-02-18 22:33:16 +0000

    see#395 made a branch for release

---
+++ branches/release5/CloudScraper/CloudScraper/ChooseDisksForm.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using System.IO;
+using CloudScraper.Properties;
+using NLog;
+using DotNetPerls;
+
+namespace CloudScraper
+{
+    public partial class ChooseDisksForm : Form
+    {
+        public static decimal totalSpaceRequired_;
+        public static List<string> selectedDisks_ = new List<string>();
+        private static Logger logger_ = LogManager.GetLogger("ChooseDisksForm");
+
+        ChooseCloudForm chooseCloudForm_;
+        NewResumeForm newResumeForm_;
+        DriveInfo[] drives_;
+        bool loaded_;
+        BindingList<VolumeInfo> volumes_;
+
+        public ChooseDisksForm(NewResumeForm newResumeForm)
+        {
+            this.loaded_ = false;
+            this.newResumeForm_ = newResumeForm;
+            this.drives_ = DriveInfo.GetDrives();
+            this.volumes_ = new BindingList<VolumeInfo>();            
+            
+            InitializeComponent();
+            
+            //Use totalSpaceRequired for use in other forms.
+            totalSpaceRequired_ = 0;
+            this.totalSpaceLabel.Text = totalSpaceRequired_ == 0 ? totalSpaceRequired_.ToString() + "GB" :
+                (totalSpaceRequired_ + Settings.Default.TotalSizeGap).ToString() + "GB";
+            
+            //Initialize basic strings UI from from settings file.
+            this.Text = Settings.Default.S2Header;
+            this.helpButton.Image = new Bitmap(Image.FromFile("Icons\\Help.png"), new Size(16, 16));
+            this.labelTotalSpace.Text = Settings.Default.S2LabelTotalSpace;
+            this.nextButton.Text = Settings.Default.S2NextButtonText;
+            this.toolTip.SetToolTip(this.nextButton, Settings.Default.S2NextButtonToolTip); 
+            this.backButton.Text = Settings.Default.S2BackButtonText;
+            this.toolTip.SetToolTip(this.backButton, Settings.Default.S2BackButtonToolTip);
+            this.toolTip.SetToolTip(this.helpButton, Settings.Default.HelpButtonToolTip);
+        }
+
+        private void BackButtonClick(object sender, EventArgs e)
+        {
+            if (logger_.IsDebugEnabled)
+                logger_.Debug("Return to NewResumeForm.");
+
+            this.Hide();
+            this.newResumeForm_.StartPosition = FormStartPosition.Manual;
+            this.newResumeForm_.Location = this.Location;
+            this.newResumeForm_.Show();
+        }
+
+        private void ChooseDisksLoad(object sender, EventArgs e)
+        {
+            if (logger_.IsDebugEnabled)
+                logger_.Debug("Form loaded.");
+            
+            //For show Form in the same position as prev.
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = this.newResumeForm_.Location;
+
+            if (!this.loaded_ && this.drives_ != null)
+            {
+                foreach (DriveInfo info in this.drives_)
+                {
+                    if (info.IsReady)
+                    {
+                        //Create VolumeInfo object.
+                        VolumeInfo volume = new VolumeInfo()
+                        {
+                            IsChecked = false,
+                            Image = new Bitmap(Image.FromFile("Icons\\HD-Drive.ico"), new Size(24, 24)),
+                            Name = info.VolumeLabel == "" ? info.DriveType + " (" + info.Name.Replace('\\', ')') :
+                                    info.VolumeLabel + " (" + info.Name.Replace('\\', ')'),
+                            ShortName = info.Name,
+                            TotalSpace = Math.Round((decimal)info.TotalSize / (1024 * 1024 * 1024), 1),
+                            UsedSpace = Math.Round((decimal)(info.TotalSize - info.TotalFreeSpace) / (1024 * 1024 * 1024), 1),
+                            FreeSpace = Math.Round((decimal)info.AvailableFreeSpace / (1024 * 1024 * 1024), 1)
+                        };
+
+                        //Check System drive.
+                        if (Environment.GetEnvironmentVariable("SystemRoot").Contains(info.Name))
+                        {
+                            volume.IsChecked = true;
+
+                            if (logger_.IsDebugEnabled)
+                                logger_.Debug("Disk " + volume.Name + " state changed to " +
+                                    volume.IsChecked.ToString());
+                            
+                            volume.Image = new Bitmap(Image.FromFile("Icons\\WindowsDrive.ico"), new Size(24, 24));
+                            totalSpaceRequired_ = volume.UsedSpace;
+                            this.nextButton.Enabled = true;
+                            this.totalSpaceLabel.Text = totalSpaceRequired_ == 0 ? Math.Round(totalSpaceRequired_, 1).ToString() + "GB" :
+                                Math.Round(totalSpaceRequired_ + Settings.Default.TotalSizeGap, 1).ToString() + "GB";
+                            selectedDisks_.Add(volume.ShortName);
+                            this.volumes_.Insert(0, volume);
+                            continue;
+                        }
+
+                        this.volumes_.Add(volume);
+                    }
+                }
+
+                //DataGrid change columns width.
+                dataGridView.DataSource = this.volumes_;
+                this.AdjustColumnOrder(this.dataGridView);
+                dataGridView.AutoResizeColumn(0);
+                dataGridView.AutoResizeColumn(1);
+
+                this.loaded_ = true;
+            }
+            else if (this.drives_ != null)
+            {
+                this.UpdateVolumes();
+            }
+        }
+
+        private void NextButtonClick(object sender, EventArgs e)
+        {
+            if (logger_.IsDebugEnabled)
+                logger_.Debug("To the ChooseCloudForm.");
+            
+            this.Hide();
+
+            if (this.chooseCloudForm_ == null)
+            {
+                this.chooseCloudForm_ = new ChooseCloudForm(this);
+            }
+
+            chooseCloudForm_.ShowDialog();
+        }
+
+        private void OnClosed(object sender, FormClosedEventArgs e)
+        {
+            this.newResumeForm_.Close();
+        }
+
+        private void UpdateVolumes()
+        {
+            this.drives_ = DriveInfo.GetDrives();
+            foreach (DriveInfo info in this.drives_)
+            {
+                if (info.IsReady)
+                {
+                    foreach (VolumeInfo volume in this.volumes_)
+                    {
+                        if (volume.ShortName == info.Name)
+                        {
+                            //Update volumme info.
+                            volume.TotalSpace = Math.Round((decimal)info.TotalSize / (1024 * 1024 * 1024), 1);
+                            volume.UsedSpace = Math.Round((decimal)(info.TotalSize - info.TotalFreeSpace) / (1024 * 1024 * 1024), 1);
+                            volume.FreeSpace = Math.Round((decimal)info.AvailableFreeSpace / (1024 * 1024 * 1024), 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        private void OnSelect(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (sender is DataGridView && e.ColumnIndex == 0)
+            {
+                this.volumes_[e.RowIndex].IsChecked = !this.volumes_[e.RowIndex].IsChecked;
+                
+                if (logger_.IsDebugEnabled)
+                    logger_.Debug("Disk " + this.volumes_[e.RowIndex].Name + " state changed to " + 
+                        this.volumes_[e.RowIndex].IsChecked.ToString());
+                
+                this.UpdateVolumes();
+
+                if (e.RowIndex == 0 && this.volumes_[0].IsChecked == false)
+                { 
+                    //Show message when system drive is unchecked.
+                    DialogResult result = BetterDialog.ShowDialog(Settings.Default.S2MessgeHeader,
+                        Settings.Default.S2MessageFirst + "\n" +
+                        Settings.Default.S2MessageSecond,"", "OK", "Cancel",
+                        Image.FromFile("Icons\\WarningDialog.png"), true);
+
+                    if (result == DialogResult.Cancel)
+                    {
+                        this.volumes_[0].IsChecked = true;
+                    }
+                    else
+                    {
+                        this.volumes_[0].IsChecked = false;
+                    }
+                }
+
+                this.nextButton.Enabled = false;
+
+                foreach (VolumeInfo vol in this.volumes_)
+                {
+                    if (vol.IsChecked)
+                    {
+                        this.nextButton.Enabled = true;
+                        break;
+                    }
+                }
+
+                totalSpaceRequired_ = 0;
+
+                if (this.nextButton.Enabled)
+                {
+                    selectedDisks_.Clear();
+
+                    foreach (VolumeInfo vol in this.volumes_)
+                    {
+                        if (vol.IsChecked)
+                        {
+                            totalSpaceRequired_ += vol.UsedSpace;
+                            selectedDisks_.Add(vol.ShortName);
+                        }
+                    }
+                }
+
+                this.totalSpaceLabel.Text = totalSpaceRequired_ == 0 ? Math.Round(totalSpaceRequired_,1).ToString() + "GB" :
+                    Math.Round(totalSpaceRequired_ + Settings.Default.TotalSizeGap, 1).ToString() + "GB";
+            }
+        }
+
+        private void HelpButtonClick(object sender, EventArgs e)
+        {
+            if (logger_.IsDebugEnabled)
+                logger_.Debug("Help button click.");
+
+            //Help button url.
+            System.Diagnostics.Process.Start(Settings.Default.S2Link);
+        }
+
+        // adjust in a right order. Needed for out-of-box .Net 2.0 for Windows Server 2003R2
+        private void AdjustColumnOrder(DataGridView view)
+        {
+            view.Columns["IsChecked"].DisplayIndex = 0;
+            view.Columns["Image"].DisplayIndex = 1;
+            view.Columns["Name"].DisplayIndex = 2;
+            view.Columns["TotalSpace"].DisplayIndex = 3;
+            view.Columns["UsedSpace"].DisplayIndex = 4;
+            view.Columns["FreeSpace"].DisplayIndex = 5;
+        }
+
+
+    }
+
+    //Class to store volume info information.
+    public class VolumeInfo
+    {   
+        [DisplayName("       ")]
+        public bool IsChecked { get; set; }
+
+        [DisplayName("        ")]
+        public Image Image { get; set; }
+        
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Browsable(false)]
+        public string ShortName { get; set; }
+
+        [DisplayName("Total Space, GB")]
+        public decimal TotalSpace { get; set; }
+
+        [DisplayName("Used Space, GB")]
+        public decimal UsedSpace { get; set; }
+
+        [DisplayName("Free Space, GB")]
+        public decimal FreeSpace { get; set; }
+    }
+}
---
+++ branches/release5/CloudScraper/CloudScraper/ToolTipContainer.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CloudScraper
+{
+    public class ToolTipContainer
+    {
+        #region Data members
+
+        private readonly List<ToolTip2Ctrl> list_;
+        private readonly ToolTip toolTip_;
+
+        #endregion Data members
+         
+        #region Constructors
+
+        public ToolTipContainer(ToolTip toolTip)
+        {
+            toolTip_ = toolTip;
+            list_ = new List<ToolTip2Ctrl>();
+        }
+
+        #endregion Constructors
+
+        #region Public methods
+
+        /// <summary>
+        /// Sets a tool tip for the control. 
+        /// 1. When the control Text property is not set, the default tool tip text is shown. 
+        /// 2. When the control Text property is set and the alternative text is not null, it 
+        /// is shown as a tool tip (empty string is a valid value). If the alternative text
+        /// is null, the control Text property is shown in the tool tip.
+        /// </summary>
+        /// <param name="ctrl">The control to set tool tip for.</param>
+        /// <param name="defaultText">A text to show in the tool tip when
+        /// the control Text property is empty.</param>
+        /// <param name="alternativeText">A text to show in the tool tip when
+        /// the control Text property is non-empty. Null means show the control
+        /// Text property in the tool tip.</param>
+        public void Create(Control ctrl, string defaultTex, string alternativeText)
+        {
+            ToolTip2Ctrl item = new ToolTip2Ctrl(toolTip_, ctrl, defaultTex, alternativeText);
+            MyRemove(item);
+            list_.Add(item);
+        }
+
+        public void Remove(Control ctrl)
+        {
+            ToolTip2Ctrl dummy = new ToolTip2Ctrl(toolTip_, ctrl, null, null);
+            MyRemove(dummy);
+        }
+
+        #endregion Public methods
+
+        #region Private methods
+
+        private void MyRemove(ToolTip2Ctrl item)
+        {
+            int removeAt = list_.FindIndex(delegate(ToolTip2Ctrl findMe)
+            {
+                return findMe.Equals(item);
+            });
+
+            if (-1 != removeAt)
+            {
+                list_[removeAt].Dispose();
+                list_.RemoveAt(removeAt);
+            }
+        }
+
+        #endregion Private methods
+
+        #region Tool tip to control mapping
+
+        private class ToolTip2Ctrl : IDisposable
+        {
+            #region Data members
+
+            private ToolTip toolTip_;
+            private Control ctrl_;
+
+            private readonly string defaultText_;
+            private readonly string alternativeText_;
+
+            #endregion Data members
+
+            /// <summary>
+            /// Sets a tool tip for the control. 
+            /// 1. When the control Text property is not set, the default tool tip text is shown. 
+            /// 2. When the control Text property is set and the alternative text is not null, it 
+            /// is shown as a tool tip (empty string is a valid value). If the alternative text
+            /// is null, the control Text property is shown in the tool tip.
+            /// </summary>
+            /// <param name="toolTip">The tool tip component.</param>
+            /// <param name="ctrl">The control to set tool tip for.</param>
+            /// <param name="defaultText">A text to show in the tool tip when
+            /// the control Text property is empty.</param>
+            /// <param name="alternativeText">A text to show in the tool tip when
+            /// the control Text property is non-empty. Null means show the control
+            /// Text property in the tool tip.</param>
+            public ToolTip2Ctrl(ToolTip toolTip, Control ctrl, string defaultText, string alternativeText)
+            {
+                toolTip_ = toolTip;
+                ctrl_ = ctrl;
+                defaultText_ = defaultText;
+                alternativeText_ = alternativeText;
+
+                ctrl_.MouseEnter += new EventHandler(OnMouseEnter);
+                ctrl_.MouseHover += new EventHandler(OnMouseHover);
+            }
+
+            #region IDisposable members
+
+            public void Dispose()
+            {
+                ctrl_.MouseEnter -= new EventHandler(OnMouseEnter);
+                ctrl_.MouseHover -= new EventHandler(OnMouseHover);
+            }
+
+            #endregion IDisposable members
+
+            #region Base overrides
+
+            public override bool Equals(object obj)
+            {
+                return (obj is ToolTip2Ctrl) ? ctrl_ == ((ToolTip2Ctrl)obj).ctrl_ : base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return null != ctrl_ ? ctrl_.GetHashCode() : base.GetHashCode();
+            }
+
+            #endregion Base overrides
+
+            #region Private methods
+
+            private void MySetToolTip()
+            {
+                string text = !string.IsNullOrEmpty(ctrl_.Text) ? alternativeText_ : defaultText_;
+                if (null == text)
+                {
+                    text = string.Empty;
+                }
+                toolTip_.SetToolTip(ctrl_, text);
+            }
+
+            private void OnMouseEnter(object sender, EventArgs e)
+            {
+                MySetToolTip();
+            }
+
+            private void OnMouseHover(object sender, EventArgs e)
+            {
+                MySetToolTip();
+            }
+
+            #endregion Private methods
+        }
+
+        #endregion Tool tip to control mapping
+    }
+}
